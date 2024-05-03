69c2c3a25f07eb6e6bb60dbf66e69dd6561e2b37
LordPachimari <opachimari@gmail.com>
2024-05-03 21:56:50 +1000

    final main branch init

--- apps/nextjs/src/middleware.ts
+++
@@ -1,9 +0,0 @@
-// Or like this if you need to do something here.
-// export default auth((req) => {
-//   console.log(req.auth) //  { session: { user: { ... } } }
-// })
-
-// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-export const config = {
-	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
