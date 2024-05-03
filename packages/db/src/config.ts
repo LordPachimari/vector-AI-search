3553796c33399486a884185c6ce2f6e7cd6a6ff6
LordPachimari <opachimari@gmail.com>
2024-05-03 21:43:14 +1000

    setting up monorepo with biomejs

---
+++ packages/db/src/config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "drizzle-kit";
+import { createEnv } from "@t3-oss/env-core";
+import * as z from "zod";
+
+const env = createEnv({
+  server: {
+    DATABASE_URL:z.string()
+  },
+  runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+});
+
+
+export default {
+  schema: "./src/schema",
+  driver: "pg",
+  dbCredentials: {
+    connectionString:env.DATABASE_URL
+  },
+  tablesFilter: ["t3turbo_*"],
+} satisfies Config;
---
+++ packages/db/src/index.ts
@@ -0,0 +1,9 @@
+
+
+export * from "drizzle-orm/sql";
+export { alias } from "drizzle-orm/mysql-core";
+
+export const schema = { };
+
+// const psClient = new Client({});
+// export const db = drizzle(psClient, { schema });
